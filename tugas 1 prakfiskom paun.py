bd4a1b5709b73a86aef83f8f267545750f690b4b
Paundra Maxi Hafiansyah <144420299+maxihafian@users.noreply.github.com>
2023-10-06 21:49:32 +0700

    Add files via upload

---
+++ tugas 1 prakfiskom paun.py
@@ -0,0 +1,15 @@
+#definisi 
+m = 2 #kg
+R = 1 #m
+v = 4 #m/s
+vk = v * v
+#percepatan sentripetal
+As = vk / R
+print (As, "m/s^2")
+#gaya sentripetal
+Fs = m * As
+print (Fs, "N")
+#kecepatan sudut
+w = v / R
+print (w, "rad/s")
+
